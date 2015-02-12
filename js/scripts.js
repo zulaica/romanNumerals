aa57d51d7fe293def8cd585f6759c27771e97777
Amy Michelle Johnson & David Zulaica <student@epicodus.com>
2015-02-11 16:49:42 -0800

    Output a Roman Numeral for any number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 var romanNumerals = function(number) {
-  // var symbols = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000};
 
   var digits = number.split("");
   var spaces = digits.length;
@@ -8,30 +7,45 @@ var romanNumerals = function(number) {
 
   var romanCalculator = function(low,mid,high) {
     if (digits[0] == 9) {
-      results = low + high;
+      results = results + low + high;
     } else if (digits[0] >= 5) {
-      results = mid;
+      results = results + mid;
       for(counter = digits[0] - 5; counter != 0; counter--) { results = results + low; }
-        // results = "D";
-        // counter = digits[0] - 5;
-        // while(counter != 0) {
-        //   results = results + "C";
-        //   counter--;
-        // }
-      } else if (digits[0] == 4) {
-        results = low + mid;
-      } else {
-        for(counter = digits[0]; counter != 0; counter--) { results = results + low; }
-        }
-    }
+    } else if (digits[0] == 4) {
+      results = results + low + mid;
+    } else {
+      for(counter = digits[0]; counter != 0; counter--) { results = results + low; }
+    };
+    digits = digits.slice(1);
+    spaces--;
+  };
+
+  if (spaces == 4) {
+    romanCalculator("M","M","M");
+  };
 
   if (spaces == 3) {
     romanCalculator("C","D","M");
-  }
+  };
 
+  if (spaces==2) {
+    romanCalculator("X","L","C");
+  };
 
-
+  if(spaces == 1) {
+    romanCalculator("I","V","X");
+  };
 
   return results;
 
 };
+
+
+// Notes:
+// ----------
+// results = "D";
+// counter = digits[0] - 5;
+// while(counter != 0) {
+//   results = results + "C";
+//   counter--;
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,22 @@
 describe("romanNumerals", function(){
-  it("returns the appropriate roman numeral for the base number", function() {
+  it("returns the appropriate roman numeral for the base number in the 100's position", function() {
     expect(romanNumerals("600")).to.equal("DC");
   });
-
+  it("returns the appropriate roman numeral for the base number in the 10's postion", function(){
+    expect(romanNumerals("60")).to.equal("LX");
+  });
+  it("returns the appropriate roman numeral for the base number in the 1's postion", function(){
+    expect(romanNumerals("6")).to.equal("VI");
+  });
+  it("returns the appropriate roman numeral for the base number in the 1000's postion", function(){
+    expect(romanNumerals("3000")).to.equal("MMM");
+  });
+  it("returns the appropriate roman numeral for any number", function(){
+    expect(romanNumerals("123")).to.equal("CXXIII");
+    expect(romanNumerals("321")).to.equal("CCCXXI");
+    expect(romanNumerals("444")).to.equal("CDXLIV");
+    expect(romanNumerals("999")).to.equal("CMXCIX");
+    expect(romanNumerals("555")).to.equal("DLV");
+    expect(romanNumerals("596")).to.equal("DXCVI");
+  });
 });
